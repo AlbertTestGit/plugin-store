b2ca8b90224f1241e84a3deb67c5fd76379ac273
Amirzhan <albert.git1@gmail.com>
2023-01-19 17:46:35 +0500

    (fix) docker support

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { License } from './license/entities/license.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'db',
       port: 5432,
       username: 'postgres',
       password: 'qwerty123',
